46026e28f876d7001905af20980b7ff81eead841
Ishraq Ahmed Jamaluddin <ishraqahmedjamaluddin@gmail.com>
2021-06-04 11:55:25 +0200

    added sections dynamically, and let the scroll sing scrollToView

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 * [Project](#project)
 
+* [Dependencies](#dependencies)
+
+* [Important files](#important_files)
+
+* [Steps to run the project](#run)
+
 ## Project
 
 This is a frontend project (landing page) that has a self-popilating navbar, each time you add a section a navbar is created for that section. With a link to lead you there.
@@ -14,3 +20,39 @@ When you're near the bottom there's a back to top button to press
 
 When you don't scroll the navbar is hidden
 
+Updates:
+The scroll is done by the scrollToView function. The sections are added dynamically as well using a for loop.
+
+## Dependencies
+
+You don't need to install any dependencies
+
+## References
+
+[W3 School](https://www.w3schools.com/)
+
+[Stack Overflow](https://stackoverflow.com/)
+
+## Important_files
+
+There are three files here:
+
+- index.html
+
+css
+- styles.css
+
+js
+- app.js
+
+The html has the skeleton of the landing page
+
+The css has the styles
+
+The javascript has most of the functionality of adding sections/ navbar items/ back to top button, and so on.
+
+## Run
+
+To run the code you just need to double click the index.html to open in a web browser.
+
+If you are a developer you can open it using live server extension in vscode
--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@
  * 
 */
 const fragment = document.createDocumentFragment();
-const sections = document.querySelectorAll('section');
+let sections;
 const mybutton = document.getElementById("myBtn");
 
 
@@ -27,6 +27,29 @@ const mybutton = document.getElementById("myBtn");
  * Start Helper Functions
  * 
 */
+(function add_sections() {
+    for (let i = 0; i < 7 ; i++) {
+        const div = document.createElement('div')
+        div.classList.add('landing__container')
+        const h2 = document.createElement('h2')
+        h2.innerText = "Section " + (i+1)
+        div.appendChild(h2)
+        const p = document.createElement('p')
+        p.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod."
+        div.appendChild(p)
+        const p2 = document.createElement('p')
+        p2.innerText = "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.";
+        div.appendChild(p2)
+        const section = document.createElement('section')
+        section.id = "section" + (i+1)
+        section.setAttribute('data-nav', "Section " + (i+1))
+        section.appendChild(div)
+        const fragment2 = document.createDocumentFragment();
+        fragment2.appendChild(section)
+        document.querySelector('main').appendChild(fragment2)
+        sections = document.querySelectorAll('section');
+    }
+})();
 
 
 /**
@@ -36,19 +59,20 @@ const mybutton = document.getElementById("myBtn");
 */
 
 // build the nav
-function add_nav() {
+(function add_nav() {
     sections.forEach(section => {
         const a = document.createElement('a');
         a.innerHTML = section.getAttribute('data-nav');
-        a.href = '#' + section.id;
         a.classList.add('menu__link')
+        a.addEventListener('click', scrolling)
+        a.style.cursor = "pointer"
         const li = document.createElement('li');
         li.id = 'li'+section.id;
         li.appendChild(a);
         fragment.appendChild(li)
     });
     document.getElementById('navbar__list').appendChild(fragment)
-}
+})();
 
 
 // Add class 'active' to section when near top of viewport
@@ -72,7 +96,7 @@ function highlight_section() {
         nav.style.display = "none";
     }, 10000);
 
-    if (window.scrollY > 3000) {
+    if (window.scrollY > 2000) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
@@ -81,7 +105,12 @@ function highlight_section() {
 mybutton.addEventListener('click', () => {document.documentElement.scrollTop = 0;})
 
 // Scroll to anchor ID using scrollTO event
-
+function scrolling(event) {
+    event.preventDefault()
+    const id = event.target.parentElement.id.substring(2)
+    const section = document.getElementById(id)
+    section.scrollIntoView()
+}
 
 /**
  * End Main Functions
@@ -89,11 +118,11 @@ mybutton.addEventListener('click', () => {document.documentElement.scrollTop = 0
  * 
 */
 
-// Build menu 
-add_nav()
+// Build menu
+// (Invoked above as IIFE)
 
 // Scroll to section on link click
-// (Already applied above)
+// (Already applied above in the add_nav())
 
 // Set sections as active
 document.addEventListener("scroll", highlight_section)
